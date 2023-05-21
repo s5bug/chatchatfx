9205cff288e3c3524d724ffaa208642a6b9eb667
Aly <me@s5.pm>
2023-05-20 22:40:32 -0700

    Fix camera

--- src/main/java/tf/bug/chatchatfx/game/WorldPlayGameApplication.java
+++ src/main/java/tf/bug/chatchatfx/game/WorldPlayGameApplication.java
@@ -23,11 +23,11 @@ import java.util.function.Consumer;
 import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.beans.binding.ObjectBinding;
-import javafx.beans.property.MapProperty;
-import javafx.beans.property.SimpleMapProperty;
+import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.MapChangeListener;
 import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.ScrollEvent;
 import tf.bug.chatchatfx.model.Facing;
@@ -45,6 +45,7 @@ public class WorldPlayGameApplication extends GameApplication {
     private final GameStateModel gameState;
 
     private final ConcurrentLinkedDeque<Throwable> errorsToThrow;
+    private Subscription preventCameraFollowFromBeingGCd; // TODO somehow get rid of this
 
     public WorldPlayGameApplication(RoomEntryModel rem, HttpClient hc, String playerName) {
         super();
@@ -88,39 +89,39 @@ public class WorldPlayGameApplication extends GameApplication {
         this.webSocket = new GameWebsocket(this.httpClient, wsUri, this.gameState, onError);
 
         ObjectBinding<PlayerModel> meNullable = this.gameState.playersProperty().valueAt(EasyBind.map(this.gameState.meIdProperty(), Number::intValue));
-        EasyBinding<PlayerModel> me = EasyBind.map(meNullable, x -> Objects.requireNonNullElseGet(x, PlayerModel::new));
-        EasyBinding<Integer> meX = me.mapObservable(PlayerModel::xProperty).map(Number::intValue);
-        EasyBinding<Integer> meY = me.mapObservable(PlayerModel::yProperty).map(Number::intValue);
+        ObservableOptionalValue<PlayerModel> me = EasyBind.wrapNullable(meNullable);
+        ObservableOptionalValue<Integer> meX = me.mapObservable(PlayerModel::xProperty).map(Number::intValue);
+        ObservableOptionalValue<Integer> meY = me.mapObservable(PlayerModel::yProperty).map(Number::intValue);
 
         FXGL.getInput().addAction(new UserAction("Move Up") {
             @Override
             protected void onAction() {
-                int hereX = meX.get();
-                int hereY = meY.get();
+                int hereX = meX.get().orElse(0);
+                int hereY = meY.get().orElse(0);
                 sendMovementPacket(hereX, hereY, Facing.UP);
             }
         }, KeyCode.UP);
         FXGL.getInput().addAction(new UserAction("Move Down") {
             @Override
             protected void onAction() {
-                int hereX = meX.get();
-                int hereY = meY.get();
+                int hereX = meX.get().orElse(0);
+                int hereY = meY.get().orElse(0);
                 sendMovementPacket(hereX, hereY, Facing.DOWN);
             }
         }, KeyCode.DOWN);
         FXGL.getInput().addAction(new UserAction("Move Left") {
             @Override
             protected void onAction() {
-                int hereX = meX.get();
-                int hereY = meY.get();
+                int hereX = meX.get().orElse(0);
+                int hereY = meY.get().orElse(0);
                 sendMovementPacket(hereX, hereY, Facing.LEFT);
             }
         }, KeyCode.LEFT);
         FXGL.getInput().addAction(new UserAction("Move Right") {
             @Override
             protected void onAction() {
-                int hereX = meX.get();
-                int hereY = meY.get();
+                int hereX = meX.get().orElse(0);
+                int hereY = meY.get().orElse(0);
                 sendMovementPacket(hereX, hereY, Facing.RIGHT);
             }
         }, KeyCode.RIGHT);
@@ -136,15 +137,11 @@ public class WorldPlayGameApplication extends GameApplication {
                         .view("missing-texture")
                         .build();
 
-                ent.setScaleX(1.0 / 16.0);
-                ent.setScaleY(1.0 / 16.0);
+                ent.setScaleX(1.0 / (16.0 * 4)); // FIXME times four due to missing texture
+                ent.setScaleY(1.0 / (16.0 * 4));
 
                 ent.xProperty().bind(EasyBind.map(change.getValueAdded().xProperty(), Number::doubleValue));
                 ent.yProperty().bind(EasyBind.map(change.getValueAdded().yProperty(), Number::doubleValue));
-
-                if(change.getKey() == this.gameState.getMeId()) {
-                    FXGL.getGameScene().getViewport().bindToEntity(ent, FXGL.getGameScene().getViewport().getWidth() / 2, FXGL.getGameScene().getViewport().getHeight() / 2);
-                }
 
                 Platform.runLater(() -> FXGL.getGameWorld().addEntity(ent));
 
@@ -152,11 +149,11 @@ public class WorldPlayGameApplication extends GameApplication {
             }
         });
 
-        double[] zooms = { 0.015625, 0.03125, 0.046875, 0.0625, 0.09375, 0.125, 0.1875, 0.25, 0.325, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 };
-        final int[] zoomLevel = {11};
+        double[] zooms = { 0.015625, 0.03125, 0.046875, 0.0625, 0.09375, 0.125, 0.1875, 0.25, 0.325, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 6.0, 8.0, 10.0, 12.0, 16.0, 20.0, 26.0, 32.0 };
+        IntegerProperty zoomLevel = new SimpleIntegerProperty(11);
         FXGL.getInput().addEventHandler(ScrollEvent.SCROLL, ev -> {
             if(ev.getDeltaX() == 0.0) {
-                int oldZoomLevel = zoomLevel[0];
+                int oldZoomLevel = zoomLevel.get();
                 int newZoomLevel = oldZoomLevel;
                 if(ev.getDeltaY() > 0) {
                     newZoomLevel = Math.max(0, Math.min(zooms.length - 1, oldZoomLevel + 1));
@@ -164,22 +161,20 @@ public class WorldPlayGameApplication extends GameApplication {
                 if(ev.getDeltaY() < 0) {
                     newZoomLevel = Math.max(0, Math.min(zooms.length - 1, oldZoomLevel - 1));
                 }
-                double newZoom = zooms[newZoomLevel] * 16.0d;
-                FXGL.getGameScene().getViewport().setZoom(newZoom);
-                zoomLevel[0] = newZoomLevel;
+                zoomLevel.set(newZoomLevel);
             }
         });
-        FXGL.getGameScene().getViewport().setZoom(zooms[zoomLevel[0]] * 16.0d);
-
-        ObjectBinding<Entity> meEntNullable = cats.valueAt(EasyBind.map(this.gameState.meIdProperty(), Number::intValue));
-        ObservableOptionalValue<Entity> meEnt = EasyBind.wrapNullable(meEntNullable);
-        ObservableOptionalValue<Point2D> meLoc =
-                meEnt.mapObservable(e ->
-                        EasyBind.combine(e.xProperty(), e.yProperty(), (x, y) -> new Point2D(x.doubleValue(), y.doubleValue())));
-        meLoc.subscribeToValues(p2d -> {
-            double w = FXGL.getGameScene().getViewport().getWidth();
-            double h = FXGL.getGameScene().getViewport().getHeight();
-            FXGL.getGameScene().getViewport().focusOn(p2d.subtract(w / 2, h / 2).add(0.5, 0.5));
+        EasyBinding<Double> zoomValue = EasyBind.map(zoomLevel, zl -> zooms[zl.intValue()] * 16.0);
+        ObservableOptionalValue<Point3D> meLoc = me.mapObservable(p ->
+                EasyBind.combine(p.xProperty(), p.yProperty(), zoomValue, (x, y, z) -> new Point3D(x.doubleValue(), y.doubleValue(), z)));
+        this.preventCameraFollowFromBeingGCd = meLoc.subscribeToValues(p3d -> {
+            Point2D center = new Point2D(p3d.getX(), p3d.getY()).add(0.5, 0.5);
+            double w = FXGL.getGameScene().getViewport().getWidth() / p3d.getZ();
+            double h = FXGL.getGameScene().getViewport().getHeight() / p3d.getZ();
+            Point2D viewPortCenter = center.subtract(w / 2.0, h / 2.0);
+            FXGL.getGameScene().getViewport().setZoom(p3d.getZ());
+            FXGL.getGameScene().getViewport().setX(viewPortCenter.getX());
+            FXGL.getGameScene().getViewport().setY(viewPortCenter.getY());
         });
     }
 
